e50525cf0d58497b72e3c3867b08d67d682bb9b7
Ambre <ambre_donnadieu@live.fr>
2020-04-14 06:14:00 +0200

    Nettoyage

--- Pluggins/UserPlugin/User.cs
+++ Pluggins/UserPlugin/User.cs
@@ -1,15 +1,10 @@
 using System.IO;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using PluginBase;
 
 namespace UserPlugin
 {
-    class User : ICommand
+    class User
     {
-        public string Name {get =>"UserPlugin";}
-        public string Description { get => "Get all user from a jsonfile into a list of user";}
         private string _Nom {get;set;}
         private string _Prenom{get;set;}
         private string _Email{get;set;}
@@ -21,18 +16,5 @@ namespace UserPlugin
             _Email = _Nom+"."+_Prenom+"@email.com"; 
         }
 
-        public int Execute()
-        {
-            List<JObject> final = new List<JObject>();
-            string pathAllJson = "./Users";
-            string[] pathAllFile = Directory.GetFiles(pathAllJson,"*.json");
-            foreach(string s in pathAllFile)
-            {
-                StreamReader file = File.OpenText(s);
-                JsonTextReader reader = new JsonTextReader(file);
-                final.Add(JToken.ReadFrom(reader) as JObject);
-            }
-            return 0;
-        }
     }
 }
--- Pluggins/UserPlugin/UserCommand.cs
+++ Pluggins/UserPlugin/UserCommand.cs
@@ -1,5 +1,9 @@
 ﻿using PluginBase;
 using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace UserPlugin
 {
@@ -11,6 +15,16 @@ namespace UserPlugin
         public int Execute()
         {
             Console.WriteLine("User !!!");
+
+            List<JObject> final = new List<JObject>();
+            string pathAllJson = "./Users";
+            string[] pathAllFile = Directory.GetFiles(pathAllJson,"*.json");
+            foreach(string s in pathAllFile)
+            {
+                StreamReader file = File.OpenText(s);
+                JsonTextReader reader = new JsonTextReader(file);
+                final.Add(JToken.ReadFrom(reader) as JObject);
+            }
             return 0;
         }
     }
